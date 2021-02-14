e774f6dd8d0507f0242ecd19a947e2f08f6720d0
nuryog <yari_3080@hotmail.com>
2021-02-14 23:31:53 +0100

    vf

--- IN.IN/2_eva/arrays.php
+++ IN.IN/2_eva/arrays.php
@@ -1,26 +1,49 @@
 <!DOCTYPE html> 
 	<html lang="es">
 	<head>
-	<title> HTML Nury 1ARI </title>
+		<title> HTML Nury 1ARI </title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maxmum-scale=2">
 	</head>
 	<body style="
-				padding-left:50px;
-				">	
-	<h1> ARRAYS </h1>
-	<div>
+				background-repeat: no-repeat;
+				background-size: cover;
+				background-image: url('rosa3.jpg');
+				text-align:center;
+				">
+		<div>
+			<h1 style="
+						color:black;
+						font-size: 3em;
+						text-decoration:underline;
+						"> 
+							
+							ARRAYSITOS
+			</h1>
+		</div>
+		<div style="border-style: solid;
+					width:900px;
+					padding-left: 50px;
+					background-color: white;
+					display:inline-block;
+					border-color: HotPink;
+					text-align:left;
+					">
 	<h2>Crea un array con el nombre de tus compañeros y selecciona uno de ellos al azar:</h2>
 	
-	</div>
+	
 	<div style="
-										height:2em;">
+										height:1.5em;">
 						</div>
 	<div id="r" style="
-						display:inline-block;
+						text-align:left;
 						">
 	</div>
 	<div style="
 										height:2em;">
 						</div>
+	<div style="
+									text-align:left;
+									">
 			<?php
 				$compis = [
 					'Andrei',
@@ -67,20 +90,35 @@
 			document.getElementById('r').
 			innerHTML= 'Compañeritos en JS: '+compis[c];
 			</script>
+			</div>
+			<div style="
+										height:2em;">
+						</div>
+			</div>
 <div style="
 										height:2em;">
 						</div>
-			<div>
+			<div style="border-style: solid;
+					width:900px;
+					padding-left: 50px;
+					background-color: white;
+					display:inline-block;
+					border-color: HotPink;
+					text-align:left;
+						">
 			
-			<h2> Almacena en un array la tabla de las potencias de 2 y muestra el contenido del array</h2>
-			</div>
+			<h2> Almacena en un array la tabla de las potencias de 2 y muestra el contenido del array:</h2>
 	<div style="
 										height:2em;">
 						</div>
-	<div id="pot" style="
+						<div style="
 						display:inline-block;
 						">
-						Holi
+	<div id="pot" style="
+						text-align:left;
+						display:inline-block;
+						">
+						Hi
 	</div>
 	<div style="
 										height:2em;">
@@ -95,11 +133,14 @@
 				}
 			?>
 
-			<table border="1" width="20%">
-					<caption>Potencias del 2 PHP</caption>
+			<table style="
+							border-style: solid;
+							width:200px;
+							">
+					<caption>Potencias del 2 PHP:</caption>
 			<?php
 				foreach ($tabla as $i=>$dato) {
-					echo "<tr><td> 2<sup>". ($i+1) ."</sup>: ". $dato . "</td></tr>";
+					echo "<tr><td> 2<sup>". ($i+1) ."</sup>:        ". $dato . "</td></tr>";
 				}
 			?>
 				</table>
@@ -116,15 +157,34 @@
 				document.getElementById('pot').
 				innerHTML= 'Potencias del 2 JS:<br/> '+txt;
 			</script>
-			
-			<div>
-			
-			<h2> Muestra el listado ordenado de meses que contengan la letra R </h2>
+			<div style="
+										height:2em;">
+						</div>
 			</div>
+		</div>
+		<div style="
+										height:2em;">
+						</div>
+			<div style="border-style: solid;
+					width:900px;
+					padding-left: 50px;
+					background-color: white;
+					display:inline-block;
+					border-color: HotPink;
+					text-align:left;
+					padding-bottom:25px;
+						">
+			
+			<h2> Muestra el listado ordenado de meses que contengan la letra R: </h2>
+			
 	<div style="
 										height:2em;">
 						</div>
+						<div style="
+						display:inline-block;
+						">
 	<div id="mes" style="
+						text-align:left;
 						display:inline-block;
 						">
 						eij
@@ -132,44 +192,43 @@
 	<div style="
 										height:2em;">
 						</div>
-			<pre>
 			<?php
 			$meses = [
-			'enero',
-			'febrero',
-			'marzo',
-			'abril',
-			'mayo',
-			'junio',
-			'julio',
-			'agosto',
-			'septiembre',
-			'octubre',
-			'noviembre',
-			'diciembre',
+			'Enero',
+			'Febrero',
+			'Marzo',
+			'Abril',
+			'Mayo',
+			'Junio',
+			'Julio',
+			'Aosto',
+			'Septiembre',
+			'Octubre',
+			'Noviembre',
+			'Diciembre',
 			];
-				echo "Meses con r:<br/>";
+			echo "Meses con r PHP: <br/>";
+			echo "<br/>";
 				foreach ($meses as $mes){
 					if ( strstr($mes,"r"))
 					echo '<div>'.$mes."</div>";
 				}
 				
 			?>
-			</pre>
 			<script>
 				meses = [
-			'enero',
-			'febrero',
-			'marzo',
-			'abril',
-			'mayo',
-			'junio',
-			'julio',
-			'agosto',
-			'septiembre',
-			'octubre',
-			'noviembre',
-			'diciembre',
+			'Enero',
+			'Febrero',
+			'Marzo',
+			'Abril',
+			'Mayo',
+			'Junio',
+			'Julio',
+			'Aosto',
+			'Septiembre',
+			'Octubre',
+			'Noviembre',
+			'Diciembre',
 			];
 			txt="";
 			for (i in meses){
@@ -179,8 +238,9 @@
 				}
 			}
 			document.getElementById("mes").
-			innerHTML= txt;	
+			innerHTML=" Meses con r JS:  <br/><br/>"+ txt;	
 			</script>
-			
+			</div>
+			</div>
 	</body>
  </html>
